a0965346d61c987d34769ae811a4f1411e8f15f2
matt <matt@atlasagency.com.au>
2017-08-07 08:56:45 +1000

    cat page update

--- src/pages/post/post.ts
+++ src/pages/post/post.ts
@@ -2,23 +2,19 @@ import { Post } from './../../providers/wp/wp';
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
-/**
- * Generated class for the PostPage page.
- *
- * See http://ionicframework.com/docs/components/#navigation for more info
- * on Ionic pages and navigation.
- */
-
 @IonicPage()
 @Component({
   selector: 'page-post',
   templateUrl: 'post.html',
 })
 export class PostPage {
-post: Post;
+  post: Post;
   constructor(public navCtrl: NavController, public navParams: NavParams) {
     this.post = navParams.get('post');
-// console.log(this.post);
+    // console.log(this.post);
+    if (this.post == undefined) {
+      this.navCtrl.setRoot('BlogPage')
+    }
 
   }
 
